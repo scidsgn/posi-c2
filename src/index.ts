08530ccfadd03369748f63bcbf8721540fdb76f1
scintilla4evr <kacper17@outlook.com>
2022-05-13 22:02:04 +0200

    Nevermind, this is final!

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,30 @@ const n = 100
 const population = rand2d(n, 2, -2.048, 2.048)
 const evalPopulation = population.map(x => evalF2(x[0], x[1]))
 
-const sigmaCutPopulation = scaleSigmaCut(evalPopulation, 1)
-const boltzmannPopulation = scaleBoltzmann(evalPopulation, 500)
-
 
 const selection = deterministicSelection(prepareCopyCounts(evalPopulation, n))
-const sigmaCutSelection = deterministicSelection(prepareCopyCounts(sigmaCutPopulation, n))
-const boltzmannSelection = deterministicSelection(prepareCopyCounts(boltzmannPopulation, n))
 
-writeFileSync(
-    "data.csv", exportToCSV({
-        "x1": population.map(xs => xs[0]),
-        "x2": population.map(xs => xs[1]),
-        "przystosowanie": evalPopulation,
-        "przystosowanie sigma-odcięcie": sigmaCutPopulation,
-        "przystosowanie boltzmann": boltzmannPopulation,
-        "selekcja bez skalowania": selection.map(e => evalPopulation[e.index]),
-        "selekcja sigma-odcięcie": sigmaCutSelection.map(e => evalPopulation[e.index]),
-        "selekcja boltzmann": boltzmannSelection.map(e => evalPopulation[e.index]),
-    }), "utf8"
-)
+const csv = {
+    "x1": population.map(xs => xs[0]),
+    "x2": population.map(xs => xs[1]),
+    "przystosowanie": evalPopulation,
+    "selekcja bez skalowania": selection.map(e => evalPopulation[e.index])
+}
+
+;[1, 2, 3].forEach(c => {
+    const sigmaCutPopulation = scaleSigmaCut(evalPopulation, c as any)
+    const sigmaCutSelection = deterministicSelection(prepareCopyCounts(sigmaCutPopulation, n))
+
+    csv[`przystosowanie sigma-odcięcie c = ${c}`] = sigmaCutPopulation
+    csv[`selekcja sigma-odcięcie c = ${c}`] = sigmaCutSelection.map(e => evalPopulation[e.index])
+})
+
+;[100, 500, 1000].forEach(t => {
+    const boltzmannPopulation = scaleBoltzmann(evalPopulation, t)
+    const boltzmannSelection = deterministicSelection(prepareCopyCounts(boltzmannPopulation, n))
+
+    csv[`przystosowanie boltzmann t = ${t}`] = boltzmannPopulation
+    csv[`selekcja boltzmann t = ${t}`] = boltzmannSelection.map(e => evalPopulation[e.index])
+})
+
+writeFileSync("data.csv", exportToCSV(csv), "utf8")
